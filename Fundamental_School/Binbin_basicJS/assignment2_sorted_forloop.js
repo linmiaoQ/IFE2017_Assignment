efb4ba3913d70ae42c6ebb85b87f8ddcd4576d8a
Q16 <Q16@QMBP.local>
2017-03-05 12:21:34 +0800

    binbin002

--- Fundamental_School/Binbin_basicJS/assignment2_sorted_forloop.js
+++ Fundamental_School/Binbin_basicJS/assignment2_sorted_forloop.js
@@ -34,20 +34,19 @@
   将空气质量指数大于60的城市显示到aqi-list的列表中
   */
 
- let aqiIndex = ['一', '二', '三', '四', '五', '六']
 
  let aqiSorted = aqiData.sort(function(a,b) {
    return b[1] - a[1];
  });
 
+ let aqiIndex = ['一', '二', '三', '四', '五', '六']
+
  let aqiList = document.getElementById('aqi-list');
 
  for (i in aqiSorted) {
-
    if (aqiSorted[i][1] >60 ){
    aqiList.innerHTML += `<li>第${aqiIndex[i]}名：${aqiSorted[i]}</li>`;
    }
-
  }
 
 })();
